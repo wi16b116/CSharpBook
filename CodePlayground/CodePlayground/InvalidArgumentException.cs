2ece3234d9cc98307e7fbb0b36209fc6754fcfeb
Ian <ianburrows.sa@gmail.com>
2017-09-22 14:36:41 +0200

    New Branch

---
+++ CodePlayground/CodePlayground/InvalidArgumentException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePlayground
+{
+    class InvalidArgumentException : ApplicationException
+    {
+        public InvalidArgumentException():base("Divide by zero error.")
+        {
+
+        }
+
+        public InvalidArgumentException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ CodePlayground/CodePlayground/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace CodePlayground
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Divide(5,0));
+            Console.ReadLine();
+        }
+
+        static double Divide(Double a, Double b)
+        {
+            if (b == 0)
+            {
+                throw new InvalidArgumentException();
+            }
+            else
+            {
+                double divide = a / b;
+                return divide;
+            }
+        }
+
+    }
+}
